1f6eb0e8296a3aea582c754c276c5eb190c52b26
MATATAT <kholdstare99@gmail.com>
2014-05-04 17:04:55 -0700

    Reworked some of the interface and changed to nuget DotNetOpenAuth

--- FaceTweet/Controllers/HomeController.cs
+++ FaceTweet/Controllers/HomeController.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DotNetOpenAuth.OAuth;
 
 namespace FaceTweet.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            try
+            {
+                return View("ValidationComplete");
+            }
+            catch (Exception)
+            {
+                return View("ValidationError");
+            }
         }
 
     }
